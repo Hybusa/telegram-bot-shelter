f7c4dc0f7406d92a02d085070d20e9527c41fe42
Anastasia <sychkova.nastyusha@mail.ru>
2023-06-08 21:47:40 +0400

    реализованы ShelterRepository и ShelterService

--- src/main/java/pro/sky/telegrambotshelter/model/Shelter.java
+++ src/main/java/pro/sky/telegrambotshelter/model/Shelter.java
@@ -1,19 +1,20 @@
 package pro.sky.telegrambotshelter.model;
 
 
-import javax.persistence.*;
-import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Objects;
 
 @Entity
-@Table(name = "shelters")
 public class Shelter {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private Long volunteerChatId;
+    private int volunteerChatId;
 
     private String shelterType;
 
@@ -29,7 +30,7 @@ public class Shelter {
 
     private String schedule;
 
-    private String howToGet;
+    private String getHowToGet;
 
     private String address;
 
@@ -45,19 +46,13 @@ public class Shelter {
 
     private String cynologistRecommendations;
 
-    private String listOfCynologists;
-
-    private String disabilityRecommendations;
-
     private String whyWeCanDeny;
 
-    @OneToMany(fetch = FetchType.EAGER, mappedBy = "shelter")
-    private List<User> users;
 
     public Shelter() {
     }
 
-    public Shelter(Long volunteerChatId,
+    public Shelter(int volunteerChatId,
                    String shelterType,
                    String meetingRecommendations,
                    String howToGetPet,
@@ -66,15 +61,13 @@ public class Shelter {
                    String phoneNumber,
                    String schedule,
                    String address,
-                   String howToGet,
+                   String getHowToGet,
                    String securityAndPass,
                    String safety,
                    String transportingRecommendations,
                    String homeRecommendationsYoung,
                    String homeRecommendationsOld,
                    String cynologistRecommendations,
-                   String listOfCynologists,
-                   String disabilityRecommendations,
                    String whyWeCanDeny) {
         this.volunteerChatId = volunteerChatId;
         this.shelterType = shelterType;
@@ -85,15 +78,13 @@ public class Shelter {
         this.phoneNumber = phoneNumber;
         this.schedule = schedule;
         this.address = address;
-        this.howToGet = howToGet;
+        this.getHowToGet = getHowToGet;
         this.securityAndPass = securityAndPass;
         this.safety = safety;
         this.transportingRecommendations = transportingRecommendations;
         this.homeRecommendationsYoung = homeRecommendationsYoung;
         this.homeRecommendationsOld = homeRecommendationsOld;
         this.cynologistRecommendations = cynologistRecommendations;
-        this.listOfCynologists = listOfCynologists;
-        this.disabilityRecommendations =  disabilityRecommendations;
         this.whyWeCanDeny = whyWeCanDeny;
     }
 
@@ -105,11 +96,11 @@ public class Shelter {
         this.id = id;
     }
 
-    public Long getVolunteerChatId() {
+    public int getVolunteerChatId() {
         return volunteerChatId;
     }
 
-    public void setVolunteerChatId(Long volunteerChatId) {
+    public void setVolunteerChatId(int volunteerChatId) {
         this.volunteerChatId = volunteerChatId;
     }
 
@@ -178,11 +169,11 @@ public class Shelter {
     }
 
     public String getGetHowToGet() {
-        return howToGet;
+        return getHowToGet;
     }
 
     public void setGetHowToGet(String getHowToGet) {
-        this.howToGet = getHowToGet;
+        this.getHowToGet = getHowToGet;
     }
 
     public String getSecurityAndPass() {
@@ -233,22 +224,6 @@ public class Shelter {
         this.cynologistRecommendations = cynologistRecommendations;
     }
 
-    public String getListOfCynologists() {
-        return listOfCynologists;
-    }
-
-    public void setListOfCynologists(String listOfCynologists) {
-        this.listOfCynologists = listOfCynologists;
-    }
-
-    public String getDisabilityRecommendations() {
-        return disabilityRecommendations;
-    }
-
-    public void setDisabilityRecommendations(String disabilityRecommendations) {
-        this.disabilityRecommendations = disabilityRecommendations;
-    }
-
     public String getWhyWeCanDeny() {
         return whyWeCanDeny;
     }
@@ -257,20 +232,12 @@ public class Shelter {
         this.whyWeCanDeny = whyWeCanDeny;
     }
 
-    public List<User> getUsers() {
-        return users;
-    }
-
-    public void setUsers(List<User> users) {
-        this.users = users;
-    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Shelter shelter = (Shelter) o;
-        return volunteerChatId.equals(shelter.volunteerChatId)
+        return volunteerChatId == shelter.volunteerChatId
                 && Objects.equals(shelterType, shelter.shelterType)
                 && Objects.equals(meetingRecommendations, shelter.meetingRecommendations)
                 && Objects.equals(howToGetPet, shelter.howToGetPet)
@@ -279,15 +246,13 @@ public class Shelter {
                 && Objects.equals(phoneNumber, shelter.phoneNumber)
                 && Objects.equals(schedule, shelter.schedule)
                 && Objects.equals(address, shelter.address)
-                && Objects.equals(howToGet, shelter.howToGet)
+                && Objects.equals(getHowToGet, shelter.getHowToGet)
                 && Objects.equals(securityAndPass, shelter.securityAndPass)
                 && Objects.equals(safety, shelter.safety)
                 && Objects.equals(transportingRecommendations, shelter.transportingRecommendations)
                 && Objects.equals(homeRecommendationsYoung, shelter.homeRecommendationsYoung)
                 && Objects.equals(homeRecommendationsOld, shelter.homeRecommendationsOld)
                 && Objects.equals(cynologistRecommendations, shelter.cynologistRecommendations)
-                && Objects.equals(listOfCynologists, shelter.listOfCynologists)
-                && Objects.equals(disabilityRecommendations, shelter.disabilityRecommendations)
                 && Objects.equals(whyWeCanDeny, shelter.whyWeCanDeny);
     }
 
@@ -302,15 +267,13 @@ public class Shelter {
                 phoneNumber,
                 schedule,
                 address,
-                howToGet,
+                getHowToGet,
                 securityAndPass,
                 safety,
                 transportingRecommendations,
                 homeRecommendationsYoung,
                 homeRecommendationsOld,
                 cynologistRecommendations,
-                listOfCynologists,
-                disabilityRecommendations,
                 whyWeCanDeny);
     }
 }
--- src/main/java/pro/sky/telegrambotshelter/repository/ShelterRepository.java
+++ src/main/java/pro/sky/telegrambotshelter/repository/ShelterRepository.java
@@ -4,16 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pro.sky.telegrambotshelter.model.Shelter;
 
-import java.util.List;
-import java.util.Optional;
-
 /**репозиторий для работы с БД приютов*/
 @Repository
 public interface ShelterRepository extends JpaRepository<Shelter, Long> {
-
-    /**метод для поиска приюта по типу*/
     Shelter findShelterByShelterType(String shelterType);
-    List<Shelter> findAllByShelterType(String shelterType);
-
-    Optional<Shelter> findShelterByVolunteerChatId(Long id);
 }

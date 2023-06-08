4e0a8d49631e848e014166c7cae362bd6491d122
Anastasia <sychkova.nastyusha@mail.ru>
2023-06-08 21:47:40 +0400

    реализованы ShelterRepository и ShelterService

--- src/main/java/pro/sky/telegrambotshelter/model/User.java
+++ src/main/java/pro/sky/telegrambotshelter/model/User.java
@@ -1,12 +1,10 @@
 package pro.sky.telegrambotshelter.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.Objects;
 
 @Entity
+@Table(name="users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)

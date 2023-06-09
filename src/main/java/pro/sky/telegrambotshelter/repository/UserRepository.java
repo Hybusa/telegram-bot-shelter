1f060be82637eaf424010aadee7afe44215b239a
Anastasia <sychkova.nastyusha@mail.ru>
2023-06-09 16:00:33 +0400

    добавлены репозитории и сервисы для сущностей pet и user

--- src/main/java/pro/sky/telegrambotshelter/repository/UserRepository.java
+++ src/main/java/pro/sky/telegrambotshelter/repository/UserRepository.java
@@ -1,19 +1,12 @@
 package pro.sky.telegrambotshelter.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pro.sky.telegrambotshelter.model.User;
-
-import java.util.List;
-
 /**репозиторий для работы с БД пользователей*/
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     /**метод для поиска пользователя по chat_id*/
     User findUserByChatId(Long id);
-
-    @Query(value = "SELECT chat_id FROM users", nativeQuery = true)
-    List<Long> listChatIdUsers();
 }

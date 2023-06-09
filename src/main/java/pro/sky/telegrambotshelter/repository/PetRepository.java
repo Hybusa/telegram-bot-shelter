b497a60cdd382861a83193374da624941715454a
Anastasia <sychkova.nastyusha@mail.ru>
2023-06-09 16:00:33 +0400

    добавлены репозитории и сервисы для сущностей pet и user

--- src/main/java/pro/sky/telegrambotshelter/repository/PetRepository.java
+++ src/main/java/pro/sky/telegrambotshelter/repository/PetRepository.java
@@ -3,14 +3,7 @@ package pro.sky.telegrambotshelter.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pro.sky.telegrambotshelter.model.Pet;
-import pro.sky.telegrambotshelter.model.Shelter;
-
-import java.util.List;
-import java.util.Optional;
-
 /**репозиторий для работы с БД животных*/
 @Repository
 public interface PetRepository extends JpaRepository<Pet, Long> {
-    List<Pet> findAllByShelter(Shelter shelter);
-    Optional<Pet> getPetByUser_ChatId(Long chatId);
 }

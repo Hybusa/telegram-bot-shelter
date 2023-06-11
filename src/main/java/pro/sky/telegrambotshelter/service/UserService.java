60c6f02ebee1f84b652d41fb8cb3ef636d27636f
Anastasia <sychkova.nastyusha@mail.ru>
2023-06-11 16:19:57 +0400

    user service

--- src/main/java/pro/sky/telegrambotshelter/service/UserService.java
+++ src/main/java/pro/sky/telegrambotshelter/service/UserService.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Service;
 import pro.sky.telegrambotshelter.model.User;
 import pro.sky.telegrambotshelter.repository.UserRepository;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
-/**сервис для работы с пользователем*/
+/**
+ * сервис для работы с пользователем
+ */
 @Service
 public class UserService {
     private final UserRepository userRepository;
@@ -17,22 +17,34 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-    /**метод для сохранения юзера в БД*/
+    /**
+     * метод для сохранения пользователя в БД
+     */
     public void save(User user) {
         userRepository.save(user);
     }
 
-    /**метод для изменения пользователя в БД*/
-    public User update(User user) {
-        return userRepository.save(user);
+    /**
+     * метод для изменения пользователя в БД
+     */
+    public void updateShelterChoiceByChatId(User user, String shelterTypeChoice){
+        Optional<User> optUser = userRepository.findUserByChatId(user.getChatId());
+        User tmpUser;
+        tmpUser = optUser.orElse(user);
+        tmpUser.setShelterTypeChoice(shelterTypeChoice);
+        userRepository.save(tmpUser);
     }
 
-    /**метод для получения контакта пользователя*/
-    public String getUserPhone(Long chatId){
-        return userRepository.findUserByChatId(chatId).getContact();
+    /**
+     * метод для получения контакта пользователя
+     */
+    public Optional<User> getUserPhone(Long chatId) {
+        return userRepository.findUserByChatId(chatId).get;
     }
 
-    /**метод для получения мапы с chat_id пользователей*/
+    /**
+     * метод для получения мапы с chat_id пользователей
+     */
     public Map<Long, String> getMapUsersChatId() {
         Map<Long, String> usersId = new HashMap<>();
         List<User> users = userRepository.findAll();
@@ -40,6 +52,49 @@ public class UserService {
             usersId.put(user.getId(), user.getShelterTypeChoice());
         }
         return usersId;
+    }
+
+    /**
+     * метод для получения пользователя по id
+     */
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findUserById(id);
+    }
+
+    /**
+     * метод для получения list с контактами пользователей приюта для собак
+     */
+    public Optional<List<String>> getListUsersContactsWithDodShelter() {
+        List<Long> usersId = new ArrayList<>(userRepository.listUsersIdFromDogsShelter());
+        List<String> contactsFromDods = new ArrayList<>();
+        String contact;
+
+        for (Long l : usersId) {
+            contact = getUserById(l).getContact();
+            if (contact != null) {
+                contactsFromDods.add(getUserById(l).getContact());
+            }
+        }
+
+        return contactsFromDods;
+    }
+
+    /**
+     * метод для получения list с контактами пользователей приюта для кошек
+     */
+    public List<String> getListUsersContactsWithCatShelter() {
+        List<Long> usersId = new ArrayList<>(userRepository.listUsersIdFromCatsShelter());
+        List<String> contactsFromCats = new ArrayList<>();
+        String contact;
+
+        for (Long l : usersId) {
+            contact = getUserById(l).getContact();
+            if (contact != null) {
+                contactsFromCats.add(getUserById(l).getContact());
+            }
+        }
+
+        return contactsFromCats;
     }
 
 }

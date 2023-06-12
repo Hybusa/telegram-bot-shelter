31d49b5893965d8c33d07166d600063ce58854b0
CosmoBlack <soulker_mozyr@yahoo.com>
2023-06-12 15:14:29 +0200

    1. добален AdoptedCats 2. реализован чат с волонтером(только для кошек)

--- src/main/java/pro/sky/telegrambotshelter/service/PetService.java
+++ src/main/java/pro/sky/telegrambotshelter/service/PetService.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Service;
 import pro.sky.telegrambotshelter.model.Pet;
 import pro.sky.telegrambotshelter.repository.PetRepository;
 
+import java.util.Collection;
+
 /**сервис для работы с БД питомцев*/
 @Service
 public class PetService {
@@ -12,6 +14,22 @@ public class PetService {
     public PetService(PetRepository petRepository) {
         this.petRepository = petRepository;
     }
+
+// для волонтера
+    /** Добавил метод получения всех питомцев приюта*/
+    public Collection<Pet> getAllPetByShelters (long shelterId) {
+       return petRepository.findAllByShelterId(shelterId);
+    }
+
+    public Pet getPetById(Long idPet) {
+        return petRepository.findPetById(idPet);
+    }
+
+    public void editPetByVolunteer(Pet pet) {
+        petRepository.save(pet);
+    }
+
+
 
 
 }

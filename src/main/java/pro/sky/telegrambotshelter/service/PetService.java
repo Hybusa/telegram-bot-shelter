d4605c4fd8fd74513f5b69682173ed8454d2791d
Hybusa <alkouzne007@gmail.com>
2023-06-16 15:33:12 +0300

    Controllers for all 3 Entities done. Some modifications to services.

--- src/main/java/pro/sky/telegrambotshelter/service/PetService.java
+++ src/main/java/pro/sky/telegrambotshelter/service/PetService.java
@@ -1,44 +1,57 @@
 package pro.sky.telegrambotshelter.service;
 
 import org.springframework.stereotype.Service;
+import org.webjars.NotFoundException;
 import pro.sky.telegrambotshelter.model.Pet;
 import pro.sky.telegrambotshelter.repository.PetRepository;
 
-import java.util.Collection;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.Optional;
 
 /**сервис для работы с БД питомцев*/
 @Service
 public class PetService {
     private final PetRepository petRepository;
+    private final ShelterService shelterService;
 
-    public PetService(PetRepository petRepository) {
+    public PetService(PetRepository petRepository, ShelterService shelterService) {
         this.petRepository = petRepository;
+        this.shelterService = shelterService;
     }
 
-    /**
-     * редактирование питомца волонтером
-     */
-    public void editPetByVolunteer(Pet pet) {
-        petRepository.save(pet);
+
+    /**Создать питомца*/
+    public Pet createPet(Pet pet) {
+       return petRepository.save(pet);
     }
 
-    /**
-     * получение всех питомцев в мапу (Id - питомец)
-     */
-    public Map<Long, Pet> getAllPetsMapIdPet() {
-
-        List<Pet> pets = petRepository.findAll();
-        Map<Long, Pet> petsShelterMap = new HashMap<>();
-
-        for (Pet pet: pets) {
-            petsShelterMap.put(pet.getId(), pet);
-        }
-
-        return petsShelterMap;
-
+    /**Обновить информацию о питомце*/
+    public Optional<Pet> updatePet(Pet pet) {
+        if(petRepository.existsById(pet.getId()))
+            return Optional.of(petRepository.save(pet));
+        return Optional.empty();
     }
 
+    /**Получить всех питомцев*/
+    public List<Pet> getAllPets() {
+        return petRepository.findAll();
+    }
+
+    /**Получить всех питомцев из конерктного приюта*/
+    public List<Pet> getAllByShelterId(Long shelterId) {
+        return petRepository.findAllByShelter(shelterService.getShelterById(shelterId)
+                .orElseThrow(() -> new NotFoundException("Shelter not found")));
+    }
+
+    /**Получить питомца по id*/
+    public Optional<Pet> getPetById(Long id) {
+        return petRepository.findById(id);
+    }
+
+    /**метод для удаления питомца из ДБ*/
+    public void deletePet(Long id){
+        if(!petRepository.existsById(id))
+            throw new NotFoundException("Pet id not found");
+        petRepository.deleteById(id);
+    }
 }

763d786b43cbb8f117f94ce312d6d7d7c8d6a450
CosmoBlack <soulker_mozyr@yahoo.com>
2023-06-19 21:22:42 +0200

    Мерж ветки vk-feature с dev

--- src/main/java/pro/sky/telegrambotshelter/repository/AdoptedCatsRepository.java
+++ src/main/java/pro/sky/telegrambotshelter/repository/AdoptedCatsRepository.java
@@ -1,16 +1,13 @@
 package pro.sky.telegrambotshelter.repository;
 
-import liquibase.pro.packaged.O;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pro.sky.telegrambotshelter.model.AdoptedCats;
 
-import java.time.LocalDateTime;
-import java.util.Collection;
 import java.util.Optional;
 
 @Repository
 public interface AdoptedCatsRepository extends JpaRepository<AdoptedCats, Long> {
-    AdoptedCats findByIdUser (Long idUser);
+    Optional<AdoptedCats> findByIdUser (Long idUser);
 
 }

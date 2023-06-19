e66a35d2756f603f4734701d8b6c9107af59d182
CosmoBlack <soulker_mozyr@yahoo.com>
2023-06-19 21:22:42 +0200

    Мерж ветки vk-feature с dev

--- src/main/java/pro/sky/telegrambotshelter/repository/AdoptedDogsRepository.java
+++ src/main/java/pro/sky/telegrambotshelter/repository/AdoptedDogsRepository.java
@@ -1,9 +1,13 @@
 package pro.sky.telegrambotshelter.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pro.sky.telegrambotshelter.model.AdoptedDogs;
 
+import java.util.Optional;
+
+@Repository
 public interface AdoptedDogsRepository extends JpaRepository<AdoptedDogs, Long> {
-    AdoptedDogs findByIdUser (Long idUser);
+    Optional<AdoptedDogs> findByIdUser (Long idUser);
 
 }

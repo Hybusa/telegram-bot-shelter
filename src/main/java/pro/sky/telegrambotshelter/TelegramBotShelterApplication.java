29d43d029cf71e53c97860e5c16257120834a167
Hybusa <alkouzne007@gmail.com>
2023-06-21 22:24:39 +0300

    Some adjustments after more testing

--- src/main/java/pro/sky/telegrambotshelter/TelegramBotShelterApplication.java
+++ src/main/java/pro/sky/telegrambotshelter/TelegramBotShelterApplication.java
@@ -7,9 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class TelegramBotShelterApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TelegramBotShelterApplication.class, args);
 	}
-
 }
